ce9f768aa45ff7cad736357b3274ab67d62f386b
Louise Oram <lcoram@gmail.com>
2018-10-23 15:17:36 +0200

    caculations to get sig2o, but not debugged

--- SCT_rewrite.c
+++ SCT_rewrite.c
@@ -9,6 +9,7 @@
 #include <gsl/gsl_rstat.h>
 #include <gsl/gsl_statistics.h>
 #include <gsl/gsl_sort.h>
+#include <gsl/gsl_blas.h>
 
 // compile: gcc -L/user/local/lib SCT_rewrite.c -lm -lgsl -lgslcblas
 
@@ -33,8 +34,8 @@ laftot= land area fraction (the user can decide whether to use it in the interpo
 */
 
 // Cristian's functions
-void spatial_consistency_test(int *box, double *boxCentre, int *numStationsInBox,
-                              double *x, double *y, double *z); // this is the interface to R (then need pointers)?
+void spatial_consistency_test(int *t2, int *box, double *boxCentre, int *numStationsInBox,
+                              double *x, double *y, double *z, double *t, double *vp); // this is the interface to R (then need pointers)?
 
 int vertical_profile_optimizer(gsl_vector *input, double **data);
 double vertical_profile_optimizer_function(const gsl_vector *v, void *data); // GSL format
@@ -44,6 +45,7 @@ void vertical_profile(int nz, double *z,
 
 
 // helpers
+gsl_matrix* invert_matrix(gsl_matrix *matrix, int size);
 double compute_quantile(double quantile, double *array, int sizeArray);
 double mean(double *array, int sizeArray);
 
@@ -128,18 +130,6 @@ int main()
   // allocate for output
   double *t_out = malloc(sizeof(double) * n);
 
-  /*
-  par<-c(mean(topt),argv$gamma.standard,5,
-       as.numeric(quantile(zopt,probs=0.1)),
-       as.numeric(quantile(zopt,probs=0.9)))
-  opt<-optim(par,tvertprof2opt)
-  tb<-tvertprof(zopt,
-              t0=opt$par[1],
-              gamma=opt$par[2],
-              a=opt$par[3],
-              h0=opt$par[4],
-              h1i=opt$par[5])
-  */
   /* data (params) that needs to be passed into vp */
   double nd = (double) n; // cast + resize to double
   // data (double *n, double *z, double *t, double *t_out)
@@ -159,38 +149,33 @@ int main()
   int status = vertical_profile_optimizer(input, data);
   printf("status: %d\n", status);
   printf ("t0: %.4f gamma: %.4f a: %.4f h0: %.4f h1i: %.4f\n",
-            gsl_vector_get (input, 0),
-            gsl_vector_get (input, 1),
-            gsl_vector_get (input, 2),
-            gsl_vector_get (input, 3),
-            gsl_vector_get (input, 4));
-  //gsl_vector_free(input);
+            gsl_vector_get(input, 0),
+            gsl_vector_get(input, 1),
+            gsl_vector_get(input, 2),
+            gsl_vector_get(input, 3),
+            gsl_vector_get(input, 4));
 
+  vertical_profile(n, z, gsl_vector_get(input, 0), gsl_vector_get(input, 1),
+    gsl_vector_get(input, 2), gsl_vector_get(input, 3), gsl_vector_get(input, 4), t_out);
+  // now have temperature profile (t_out)
+  gsl_vector_free(input);
 
   // variables for SCT
-  // 266;-247429.070909252;-365421.526660934;3466;
+  int t2 = 25;
+    // 266;-247429.070909252;-365421.526660934;3466;
   int box = 266;
   double boxCentre[2];
   boxCentre[0] = -247429.070909252;
   boxCentre[1] = -365421.526660934;
   int numStationsInBox = n;
-    /*
-  int nmin=50;
-  int dzmin=30;
-  int dhmin=10000;
-  int dz=200;
-  int dz_bg=1500;
-  double eps2_bg=0.5;
-  double *eps2 =
-  double *T2 =
-  */
-  // bvoid spatial_consistency_test(int *box, double *boxCentre, int *numStationsInBox,
-                                //double *x, double *y, double *z)
 
+  // void spatial_consistency_test(int *t2, int *box, double *boxCentre, int *numStationsInBox,
+                                //double *x, double *y, double *z, double *vp)
   clock_t start = clock(), diff;
-  spatial_consistency_test(&box, boxCentre, &numStationsInBox, z, x, y);
+  spatial_consistency_test(&t2, &box, boxCentre, &numStationsInBox, z, x, y, t, t_out);
   diff = clock() - start;
   int msec = diff * 1000 / CLOCKS_PER_SEC;
+  printf("SCT end\n");
   printf("Time taken %d seconds %d milliseconds \n", msec/1000, msec%1000);
 
   free(t);
@@ -236,8 +221,7 @@ int vertical_profile_optimizer(gsl_vector *input, double **data)
         {
           printf ("converged to minimum at\n");
         }
-
-      printf ("iter= %5d f()= %10.3e size= %.3f\n", iter, s->fval, size);
+      //printf ("iter= %5d f()= %10.3e size= %.3f\n", iter, s->fval, size);
       /*
       printf ("t0: %.4f gamma: %.4f a: %.4f h0: %.4f h1i: %.4f\n",
               gsl_vector_get (s->x, 0),
@@ -336,8 +320,7 @@ void vertical_profile(int nz, double *z,
       t_out[i] = t0-gamma*z[i];
     }
     if(z_in) {
-      t_out[i] = t0-gamma*z[i]-a/2*
-        (1+cos(M_PI*(z[i]-h0)/(h1-h0)));
+      t_out[i] = t0-gamma*z[i]-a/2*(1+cos(M_PI*(z[i]-h0)/(h1-h0)));
     }
   }
 }
@@ -372,33 +355,30 @@ void vertical_profile(int nz, double *z,
 #   been applied; (ii) -1 if just one station in the domain
 #
 */
-void spatial_consistency_test(int *box, double *boxCentre, int *numStationsInBox,
-                              double *x, double *y, double *z)
+void spatial_consistency_test(int *t2, int *box, double *boxCentre, int *numStationsInBox,
+                              double *x, double *y, double *z, double *t, double *vp)
                               //int *nmin, int *dzmin, int *dhmin, int *dz,
                               //int *dz_bg, double *eps2_bg, double *eps2,
                               //double *T2)
 {
   int n = numStationsInBox[0];
-  printf("SCT - number stations %i \n", n);
+  printf("SCT - number stations %i in box %i \n", n, box[0]);
   /*
   # distance matrices
-  disth<-(outer(xtot[j],xtot[j],FUN="-")**2.+
-        outer(ytot[j],ytot[j],FUN="-")**2.)**0.5
+  disth<-(outer(xtot[j],xtot[j],FUN="-")**2.+ outer(ytot[j],ytot[j],FUN="-")**2.)**0.5
   distz<-abs(outer(ztot[j],ztot[j],FUN="-"))
   */
   double** disth = malloc(sizeof(double*)*n);
   double** distz = malloc(sizeof(double*)*n);
-  printf("allocated matrices \n");
   // no need to select j since already only have those for a particular box
-  // TODO: is this equation in the paper??
   // outer product of the matrices
   for(int i=0; i<n; i++) {
     disth[i] = malloc(sizeof(double)*n);
     distz[i] = malloc(sizeof(double)*n);
     for(int j=0; j<n; j++) {
-          //printf("i %i j %i \n", i, j);
-          disth[i][j] = pow((pow((x[i]-x[j]),2)+pow((y[i]-y[j]),2)),0.5);
-          distz[i][j] = abs(z[i]-z[j]);
+      //printf("i %i j %i \n", i, j);
+      disth[i][j] = pow((pow((x[i]-x[j]),2)+pow((y[i]-y[j]),2)),0.5);
+      distz[i][j] = abs(z[i]-z[j]);
     }
   }
   /*
@@ -411,12 +391,162 @@ void spatial_consistency_test(int *box, double *boxCentre, int *numStationsInBox
   # background error correlation matrix
   S = exp(-0.5*(disth/Dh)**2.-0.5*(distz/Dz)**2.)
   */
-  //Dh = average()
+  // for each station what is the 10 percentile of the distance between that station and all the others
+  // for all the stations and take the mean
+  double Dhmin = 10000;
+  double Dzmin = 30;
+  int Dz = 200; // good value (use this default)
+  // use these defaults for now, TODO: compute
+
+
+  //double** S = malloc(sizeof(double*)*n);
+  gsl_matrix *S, *Sinv;
+  S = gsl_matrix_alloc(n,n);
+  Sinv = gsl_matrix_alloc(n,n);
+  for(int i=0; i<n; i++) {
+    //S[i] = malloc(sizeof(double)*n);
+    for(int j=0; j<n; j++) {
+      //S[i][j] = exp(-.5*pow((disth[i][j]/Dhmin),2)-.5*pow((distz[i][j]/Dzmin),2));
+      double value = exp(-.5*pow((disth[i][j]/Dhmin),2)-.5*pow((distz[i][j]/Dzmin),2));
+      if(i == j) { // weight the diagonal (0.5 default)
+        //gsl_matrix_diagonal(S_gsl);
+        //value = value *0.5;
+      }
+      gsl_matrix_set(S,i,j,value);
+      gsl_matrix_set(Sinv,i,j,value); // not yet inverted, but need a copy of S
+    }
+  }
+  printf("created the S matrix\n");
+
+
+  bool first = true;
+  int stationFlags[n];
+  //  d<-topt-tb
+  gsl_vector *d;
+  d = gsl_vector_alloc(n);
+  for(int i=0; i<n; i++) {
+    // initialize with 0 (flag no stations)
+    stationFlags[i] = 0;
+    gsl_vector_set(d,i,(t[i]-vp[i])); // difference between temp and temperature from vertical profile
+  }
+
+  // now loop for SCT
+  while(1) {
+
+    if(first) {
+      // if first time then invert matrix
+      //gsl_matrix* invert_matrix(gsl_matrix *matrix, int size)
+      clock_t start = clock(), diff;
+      //gsl_matrix *S_gsl_inv = invert_matrix(S_gsl,n);
+      gsl_linalg_cholesky_invert(Sinv); // in place
+      diff = clock() - start;
+      int msec = diff * 1000 / CLOCKS_PER_SEC;
+      printf("Time taken to invert matrix %d seconds %d milliseconds \n", msec/1000, msec%1000);
+      first = false;
+    }
+    else { // not first time
+      // Update inverse matrix (Uboldi et al 2008, Appendix AND erratum!)
+      /*
+      aux<-SRinv
+      SRinv<-aux[-indx,-indx]-(tcrossprod(aux[indx,-indx],aux[-indx,indx]))*Zinv[indx]
+      S<-S[-indx,-indx]
+      eps2.vec<-eps2.vec[-indx]
+      rm(aux)
+      */
+      // take out the stations that we have decided to remove
+
+
+
+    }
+
+    gsl_vector_view diagSinv = gsl_matrix_diagonal(Sinv); // diagonal of Sinv
+    gsl_vector *Zinv, *Sinv_d, *ares_temp, *ares;
+    Zinv = gsl_vector_alloc(n);
+    Sinv_d = gsl_vector_alloc(n);
+    ares_temp = gsl_vector_alloc(n);
+    ares = gsl_vector_alloc(n);
+    // (SRinv.d<-crossprod(SRinv,d[sel]))
+    gsl_blas_dgemv(CblasNoTrans, 1, Sinv, d, 1, Sinv_d); // crossprod Sinv & d to create Sinv_d
+    gsl_blas_dgemv(CblasNoTrans, 1, S, Sinv_d, 1, ares_temp); // crossprod S and Sinv_d to create ares_temp
+    for(int i=0; i<n; i++) {
+      gsl_vector_set(Zinv,i,(1/gsl_vector_get(&diagSinv.vector,i))); //Zinv<-1/diag(SRinv)
+      gsl_vector_set(ares,i,(gsl_vector_get(ares_temp,i)-gsl_vector_get(d,i))); // ares<-crossprod(S,SRinv.d)-d[sel]
+    }
+    gsl_vector_free(ares_temp);
+
+    // cvres<--Zinv*SRinv.d
+    gsl_vector *cvres;
+    cvres = gsl_vector_alloc(n);
+    for(int i=0; i<n; i++) {
+      gsl_vector_set(cvres,i,-1*gsl_vector_get(Zinv,i));
+    }
+    gsl_vector_mul(cvres,Sinv_d); // multiplies -Zinv(initial cvres) by Sinv_d (result stored in cvres)
+
+    // sig2o<-mean(d[sel]*(-ares))
+    gsl_vector *sig2o_temp, *negAres_temp;
+    sig2o_temp = gsl_vector_alloc(n);
+    negAres_temp = gsl_vector_alloc(n);
+    for(int i=0; i<n; i++) {
+      gsl_vector_set(negAres_temp,i,-1*gsl_vector_get(ares,i));
+    }
+    gsl_vector_memcpy(sig2o_temp,d); // copies d into sig2o_temp
+    gsl_vector_mul(sig2o_temp,negAres_temp); // multiplies d by -ares
+    double sig2o = 0;
+    for(int i=0; i<n; i++) {
+      sig2o = sig2o + gsl_vector_get(sig2o_temp,i);
+    }
+    sig2o = sig2o/n;
+    printf("sig2o: %f\n", sig2o);
+    gsl_vector_free(sig2o_temp);
+    gsl_vector_free(negAres_temp);
+
+    // pog[sel]<-(ares*cvres)/sig2o
+    gsl_vector *pog, *pog_temp;
+    pog = gsl_vector_alloc(n);
+    pog_temp = gsl_vector_alloc(n);
+    gsl_vector_memcpy(pog_temp,ares); // copies ares into pog_temp
+    gsl_vector_mul(pog_temp,cvres); // multiplies ares by cvres
+    //printf("pog: ");
+    for(int i=0; i<n; i++) {
+      gsl_vector_set(pog,i,(gsl_vector_get(pog_temp,i)/sig2o));
+      //printf("%f", gsl_vector_get(pog,i));
+    }
+    //printf("\n");
+    gsl_vector_free(pog_temp);
+
+    // figure out if we should flag a station
+    int throwOut = 0;
+    for(int i=0; i<n; i++) {
+      // does it fail the test
+      if(gsl_vector_get(pog,i) > t2[0]) {
+        //printf("throw out this piece of data: %f\n", gsl_vector_get(pog,i));
+        throwOut = throwOut + 1;
+      }
+    }
+    printf("throw out: %i \n",throwOut);
+
+  break;
+  }
+  // end of while SCT loop
+
+
+
+  // FREE ALL THE MEMORY !!!
+  //gsl_matrix_free(S);
 
 }
 
 //----------------------------------------------------------------------------//
 // HELPER FUNCTIONS
+gsl_matrix* invert_matrix(gsl_matrix *matrix, int size)
+{
+  gsl_permutation *p = gsl_permutation_alloc(size);
+  gsl_matrix *matrix_inv = gsl_matrix_alloc(size,size);
+  gsl_linalg_LU_invert(matrix,p,matrix_inv);
+  gsl_permutation_free(p);
+  return matrix_inv;
+}
+
 double compute_quantile(double quantile, double *array, int sizeArray)
 {
   // compute quantiles
@@ -435,8 +565,8 @@ double compute_quantile(double quantile, double *array, int sizeArray)
 
   /* estimated values */
   val_q = gsl_rstat_quantile_get(work_q);
-  printf ("%.5f quartile: exact = %.5f, estimated = %.5f, error = %.6e\n",
-          quantile, exact_q, val_q, (val_q - exact_q) / exact_q);
+  //printf ("%.5f quartile: exact = %.5f, estimated = %.5f, error = %.6e\n",
+  //        quantile, exact_q, val_q, (val_q - exact_q) / exact_q);
   gsl_rstat_quantile_free(work_q);
   return exact_q;
 }
@@ -449,6 +579,6 @@ double mean(double *array, int sizeArray)
     sum = sum + array[i];
   }
   double mean = sum/sizeArray;
-  printf("Mean: %f\n", mean);
+  //printf("Mean: %f\n", mean);
   return mean;
 }
